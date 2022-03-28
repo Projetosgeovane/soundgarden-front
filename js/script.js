30880984e04a373f6dd4c8b469a714bf714f70bd
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-03-27 22:09:08 -0300

    Script.js

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+
+
+const inputNome = document.querySelector('#nome');
+const inputBanner = document.querySelector('#banner');
+const inputAtracoes = document.querySelector('#atracoes');
+const inputDescricao = document.querySelector('#descricao');
+const inputData = document.querySelector('#data');
+const inputLotacao = document.querySelector('#lotacao');
+const form = document.querySelector('.col-6');
+
+
+// Função cadastrar evento
+form.onsubmit = async (evento) => {
+    evento.preventDefault();
+
+
+
+    try {
+
+
+        const newEvento = {
+            name: inputNome.value,
+            poster: inputBanner.value,
+            attractions: inputAtracoes.value.split(','),
+            description: inputDescricao.value,
+            scheduled: "2022-03-24T00:57:37.761Z",
+            number_tickets: parseInt(inputLotacao.value)
+        }
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify(newEvento),
+            Headers: {
+                "Content-type": "application/json",
+            },
+        }
+
+
+
+        const resultado = await fetch(`${BASE_URL}/events`, options);
+        const conteudoResultado = await resultado.json();
+        console.log(conteudoResultado);
+    } catch {
+        alert('deu ruim');
+    }
+
+    // const dateTime = inputData.value;
+    // const [day, month, yearTime] = dateTime.split('/');
+    // let convertedDate = [month, day, yearTime].join('/');
+    // convertedDate = new Date(convertedDate).toISOString();
+
+}
+
+
+
+
+
+
