c08159deef1b96963c2e5e18c9f2c6a988e89268
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-03-29 19:39:23 -0300

    Inserindo 1o protótipo do Carrossel de Banners

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// modal
 const modal = document.querySelector(".modal");
 const btns = document.querySelectorAll(".modal-botao"); 
 const span = document.querySelector(".close");
@@ -14,4 +15,52 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// carrossel
+const slides = document.getElementsByClassName("carrousel-item")
+let slidePosition = 0
+const totalSlides = slides.length
+
+document.getElementById("carrousel-button-prev").addEventListener("click", () => {
+    moveSlide("previous")
+})
+document.getElementById("carrousel-button-next").addEventListener("click", () => {
+    moveSlide("next")
+})
+
+function hideAllSlides() {
+    for(const slide of slides) {
+        slide.classList.remove("carrousel-item-visible")
+    }
+}
+
+function showCurrentSlide() {
+    slides[slidePosition].classList.add("carrousel-item-visible")
+}
+
+function movePreviousPosition() {
+    if(slidePosition > 0) {
+        slidePosition--
+    } else {
+        slidePosition = totalSlides - 1
+    }
+}
+
+function moveNextPosition() {
+    if(slidePosition < totalSlides - 1) {
+        slidePosition++
+    } else {
+        slidePosition = 0
+    }
+}
+
+function moveSlide(direction) {
+    hideAllSlides()
+    if(direction === "previous") {
+        movePreviousPosition()
+    } else if(direction === "next") {
+        moveNextPosition()
+    }
+    showCurrentSlide()
 }
