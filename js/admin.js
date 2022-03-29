a78b3c9d7dd765ef5ca186f05ffe70bc94af893b
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-03-29 00:07:10 -0300

    Excluindo Evento por ID

--- js/admin.js
+++ js/admin.js
@@ -1,29 +1,60 @@
-let url = window.location.pathname;
-let arquivoHtml = url.substring(url.lastIndexOf('/') + 1);
 BASE_URL = 'https://xp41-soundgarden-api.herokuapp.com';
 
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow',
+//     headers: {
+//         "Content-type": "application/json;"
+//     }
+// };
 
+// let conteudoResultado = document.querySelector('#lista-eventos');
 
-if (arquivoHtml === 'admin.html') {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        headers: {
-            "Content-type": "application/json;"
-        }
-    };
+// fetch(`${BASE_URL}/events`, requestOptions)
+//     .then(response => response.json())
+//     .then((result) => {
+//         result.forEach((evento, index) => {
+//             // console.log(evento._id)
+//             let data = evento.scheduled.slice(0, 10).split('-').reverse().join('/')
+//             let hora = evento.scheduled.slice(11, 19)
+//             conteudoResultado.innerHTML +=
+//                 `<tr>
+//                 <th scope="row">${index +1}</th>
+//                 <td>${data}<br/>${hora} </td>
+//                 <td>${evento.name}</td>
+//                 <td>${evento.attractions}</td>
+//                 <td>
+//                     <a href="reservas.html?id=${evento._id}" class="btn btn-dark">ver reservas</a>
+//                     <a href="editar-evento.html?id=${evento._id}" class="btn btn-secondary">editar</a>
+//                     <a href="excluir-evento.html?id=${evento._id}" class="btn btn-danger">excluir</a>
+//                 </td>
+//             </tr>`
+//         });
+//         // console.log(conteudoResultado)
+//     })
+//     .then(
 
-    let conteudoResultado = document.querySelector('#lista-eventos');
+//     )
+//     .catch(error => console.log('error', error));
 
-    fetch(`${BASE_URL}/events`, requestOptions)
-        .then(response => response.json())
-        .then((result) => {
-            result.forEach((evento, index) => {
-                // console.log(evento._id)
-                let data = evento.scheduled.slice(0, 10).split('-').reverse().join('/')
-                let hora = evento.scheduled.slice(11, 19)
-                conteudoResultado.innerHTML +=
-                    `<tr>
+const exibirEvento = async () => {
+    try {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                "Content-type": "application/json;"
+            }
+        };
+
+        let conteudoResultado = document.querySelector('#lista-eventos');
+
+        const eventos = await (await fetch(`${BASE_URL}/events`, requestOptions)).json()
+        eventos.forEach((evento, index) => {
+            const data = evento.scheduled.slice(0, 10).split('-').reverse().join('/')
+            const hora = evento.scheduled.slice(11, 19)
+            conteudoResultado.innerHTML +=
+                `<tr>
                     <th scope="row">${index +1}</th>
                     <td>${data}<br/>${hora} </td>
                     <td>${evento.name}</td>
@@ -34,17 +65,13 @@ if (arquivoHtml === 'admin.html') {
                         <a href="excluir-evento.html?id=${evento._id}" class="btn btn-danger">excluir</a>
                     </td>
                 </tr>`
-            });
-            // console.log(conteudoResultado)
-        })
-        .then(
+        });
 
-        )
-        .catch(error => console.log('error', error));
+
+    } catch {
+        console.log('Falha de Conexão Erro 404')
+
+    }
 }
 
-// if (url.includes('/excluir-evento.html/:6240c5efb8b5fc9f101a0307')) {
-//     console.log("kkk")
-// }
-
-// console.log(url.includes('/:'))
+exibirEvento();
---
+++ js/excluir-evento.js
@@ -0,0 +1,57 @@
+const urlParametros = new URLSearchParams(window.location.search);
+const idEvento = urlParametros.get('id');
+const formDeletar = document.querySelector('form');
+BASE_URL = 'https://xp41-soundgarden-api.herokuapp.com';
+
+const deletarEvento = async () => {
+    const requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8 "
+        }
+    };
+
+    try {
+        await fetch(`${BASE_URL}/events/${idEvento}`, requestOptions)
+        alert("Evento excluído com sucesso!")
+        window.location.pathname = 'admin.html'       
+
+    } catch {
+        alert("ID Não encontrado!")
+    }
+}
+
+
+const exibirEvento = async () => {
+    try {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8 "
+            }
+        };
+
+        const evento = await (await fetch(`${BASE_URL}/events/${idEvento}`, requestOptions)).json()
+
+        const data = evento.scheduled.slice(0, 10).split('-').reverse().join('/');
+        const hora = evento.scheduled.slice(11, 19);
+        document.querySelector('#nome').value = evento.name;
+        document.querySelector('#banner').value = evento.poster;
+        document.querySelector('#atracoes').value = evento.attractions;
+        document.querySelector('#descricao').value = evento.description;
+        document.querySelector('#data').value = `${data} ${hora}`;
+        document.querySelector('#lotacao').value = evento.number_tickets;
+    } catch {
+        alert("ID Não encontrado!")
+    }
+}
+
+formDeletar.onsubmit = (event) => {
+    event.preventDefault();
+    deletarEvento();
+}
+
+
+exibirEvento();
