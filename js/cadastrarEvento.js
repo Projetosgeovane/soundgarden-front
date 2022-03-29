614f52c286302db5954905806998101a8f90ad53
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-03-28 23:57:37 -0300

    função cadastrar com tratamento de erros

--- js/cadastrarEvento.js
+++ js/cadastrarEvento.js
@@ -10,49 +10,45 @@ const form = document.querySelector('.col-6');
 
 
 // Função cadastrar evento
-form.onsubmit = async (evento) => {
-    evento.preventDefault();
-    
+try {
+    form.onsubmit = async (evento) => {
+        evento.preventDefault();
+
         const dateTime = inputData.value;
         const [day, month, yearTime] = dateTime.split('/');
         let convertedDate = [month, day, yearTime].join('/');
         convertedDate = new Date(convertedDate).toISOString();
 
-    const newEvento = {
-        name: inputNome.value,
-        poster: inputBanner.value,
-        attractions: inputAtracoes.value.split(','),
-        description: inputDescricao.value,
-        scheduled: convertedDate,
-        number_tickets: parseInt(inputLotacao.value)
-    }
-
-
-    // fetch(`${BASE_URL}/events`, {
-    //     method: "POST",
-    //     body: JSON.stringify(newEvento),
-    //     headers: { "Content-type": "application/json; charset=UTF-8" }
-    // })
-    //     .then(response => response.json()) // converter para json
-    //     .then(json => console.log(json)); //imprimir dados no console
-
-
-    const options = {
-        method: "POST",
-        body: JSON.stringify(newEvento),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8 "
+        const newEvento = {
+            name: inputNome.value,
+            poster: inputBanner.value,
+            attractions: inputAtracoes.value.split(','),
+            description: inputDescricao.value,
+            scheduled: convertedDate,
+            number_tickets: parseInt(inputLotacao.value)
         }
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify(newEvento),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8 "
+            }
+        }
+
+        const resultado = await fetch(`${BASE_URL}/events`, options)
+
+        const conteudoResultado = await resultado.json(); // converter para json
+        console.log(conteudoResultado); //imprimir dados no console  
+        alert("Evento cadastrado com sucesso!")
     }
-
-    const resultado = await fetch(`${BASE_URL}/events`, options)
-
-    const conteudoResultado = await resultado.json(); // converter para json
-    console.log(conteudoResultado); //imprimir dados no console  
-
-
-
+} catch{
+    alert("Erro ao cadastrar evento!");
+    
 }
+
+
+
 
 
 
