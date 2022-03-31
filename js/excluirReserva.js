364f9d9b3d2dfda0f7f5d34974e379c334375d47
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-03-30 21:56:43 -0300

    função excluir e reserva.js

--- js/excluirReserva.js
+++ js/excluirReserva.js
@@ -1,12 +1,51 @@
 const urlParametros = new URLSearchParams(window.location.search);
-const idReserva = urlParametros.get('id');
+const idEvento = urlParametros.get('id');
+const idReserva = urlParametros.get('idReserva');
 const formDeletarReserva = document.querySelector('form');
 const BASE_URL = 'https://xp41-soundgarden-api.herokuapp.com';
+const conteudoResultado = document.querySelector('#lista-eventos');
+const inputIngresso = document.querySelector('#ingressos');
+
+
+
+const retornarIngressos = async () => {
+
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: {
+            "Content-type": "application/json;"
+        }
+    };
+
+
+    const reservas = await (await fetch(`${BASE_URL}/bookings/event/${idEvento}`, requestOptions)).json()
+    console.log(reservas);
+
+    reservas.forEach((evento) => {
+
+        ingressosTotal = evento.event.number_tickets;
+
+
+    });
+}
 
 
 formDeletarReserva.onsubmit = async (event) => {
     try {
         event.preventDefault();
+
+        const requestOptionsGet = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                "Content-type": "application/json;"
+            }
+        };
+
+        const reserva = await (await fetch(`${BASE_URL}/bookings/${idReserva}`, requestOptionsGet)).json()
+        
+        const ingressosReserva = reserva.number_tickets;
 
         const requestOptions = {
             method: 'DELETE',
@@ -17,6 +56,32 @@ formDeletarReserva.onsubmit = async (event) => {
         };
 
         await fetch(`${BASE_URL}/bookings/${idReserva}`, requestOptions)
+
+        ingressosTotal += ingressosReserva;
+        console.log(ingressosTotal)
+
+
+
+
+
+
+        const data = {
+            
+            number_tickets: ingressosTotal
+
+        }
+
+        const options = {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+        const requisicao = await fetch(`${BASE_URL}/events/${idEvento}`, options)
+        const conteudorequisicao = await requisicao.json(data)
+        console.log(conteudorequisicao)
+
         alert("Evento excluído com sucesso!")
         location.replace(document.referrer);
 
@@ -28,7 +93,7 @@ formDeletarReserva.onsubmit = async (event) => {
 const exibirReserva = async () => {
     try {
 
-        const requestOptions = {
+        const requestOptionsGet = {
             method: 'GET',
             redirect: 'follow',
             headers: {
@@ -36,14 +101,21 @@ const exibirReserva = async () => {
             }
         };
 
-        const reserva = await (await fetch(`${BASE_URL}/bookings/${idReserva}`, requestOptions)).json()
+        const reserva = await (await fetch(`${BASE_URL}/bookings/${idReserva}`, requestOptionsGet)).json()
 
         document.querySelector('#nome').value = reserva.owner_name;
         document.querySelector('#email').value = reserva.owner_email;
         document.querySelector('#ingressos').value = reserva.number_tickets;
+
     } catch {
         alert("ID Não encontrado!")
     }
 }
 
+
+
+
+
+
+retornarIngressos();
 exibirReserva();
