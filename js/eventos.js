f02524926cded0ae133eb5a3f5382cc48ea9a83a
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-03-31 18:05:30 -0300

    atualizando a landing page e a página de eventos

--- js/eventos.js
+++ js/eventos.js
@@ -1,56 +1,5 @@
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-// carrossel
-const slides = document.getElementsByClassName("carrousel-item");
-let slidePosition = 0;
-const totalSlides = slides.length;
-
-document
-	.getElementById("carrousel-button-prev")
-	.addEventListener("click", () => {
-		moveSlide("previous");
-	});
-document
-	.getElementById("carrousel-button-next")
-	.addEventListener("click", () => {
-		moveSlide("next");
-	});
-
-function hideAllSlides() {
-	for (const slide of slides) {
-		slide.classList.remove("carrousel-item-visible");
-	}
-}
-
-function showCurrentSlide() {
-	slides[slidePosition].classList.add("carrousel-item-visible");
-}
-
-function movePreviousPosition() {
-	if (slidePosition > 0) {
-		slidePosition--;
-	} else {
-		slidePosition = totalSlides - 1;
-	}
-}
-
-function moveNextPosition() {
-	if (slidePosition < totalSlides - 1) {
-		slidePosition++;
-	} else {
-		slidePosition = 0;
-	}
-}
-
-function moveSlide(direction) {
-	hideAllSlides();
-	if (direction === "previous") {
-		movePreviousPosition();
-	} else if (direction === "next") {
-		moveNextPosition();
-	}
-	showCurrentSlide();
-}
 
 //pegar lista de eventos
 const eventosIndex = async (index) => {
@@ -67,8 +16,8 @@ const eventosIndex = async (index) => {
 	}
 };
 
-eventosIndex(3).then((resp) => {
-	const cardsEventos = document.querySelector("#container-eventos-index");
+eventosIndex(18).then((resp) => {
+	const cardsEventos = document.querySelector("#container-eventos");
 
 	console.log(resp);
 
