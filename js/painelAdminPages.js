86f6a1be176f90568206305bd3a00abc71698e24
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-03-31 21:00:35 -0300

    Correções Painel administrativo

--- js/painelAdminPages.js
+++ js/painelAdminPages.js
@@ -4,9 +4,7 @@ let largura = window.innerWidth ||
 
 
 if (largura < 911) {
-    const painelADMBtn = document.querySelector('#button-painel-adm');
-    painelADMBtn.style.display = 'block'
-
+    
     const painelADMInput = document.querySelector('#input-menu');
     if (painelADMInput.checked === false) {
         painelADMInput.checked = true;
